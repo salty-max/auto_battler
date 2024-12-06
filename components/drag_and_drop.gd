2274b894ea25477c7797d87f7ba1f9a3bf21dc5c
salty-max <max@jellycat.fr>
2024-12-06 21:38:43 +0100

    rotation during drag + outline highlighter

--- components/drag_and_drop.gd
+++ components/drag_and_drop.gd
@@ -23,6 +23,13 @@ func _process(_delta: float) -> void:
 		target.global_position = target.get_global_mouse_position() + offset
 
 
+func _input(event: InputEvent) -> void:
+	if dragging and event.is_action_pressed("cancel"):
+		_cancel_dragging()
+	elif dragging and event.is_action_released("select"):
+		_drop()
+
+
 func _start_dragging() -> void:
 	dragging = true
 	starting_pos = target.global_position
@@ -57,9 +64,5 @@ func _on_target_input_event(_viewport: Node, event: InputEvent) -> void:
 	if not dragging and dragging_object:
 		return
 
-	if dragging and event.is_action_pressed("cancel"):
-		_cancel_dragging()
-	elif not dragging and event.is_action_pressed("select"):
+	if not dragging and event.is_action_pressed("select"):
 		_start_dragging()
-	elif dragging and event.is_action_released("select"):
-		_drop()
--- scenes/unit/unit.gd
+++ scenes/unit/unit.gd
@@ -4,9 +4,20 @@ extends Area2D
 
 @export var stats: UnitStats : set = set_stats
 
-@onready var skin: AnimatedSprite2D = $Skin
-@onready var health_bar: ProgressBar = $GUI/HealthBar
-@onready var mana_bar: ProgressBar = $GUI/ManaBar
+@onready var skin: AnimatedSprite2D = %Skin
+@onready var shadow: Sprite2D = $Visuals/Shadow
+@onready var health_bar: ProgressBar = %HealthBar
+@onready var mana_bar: ProgressBar = %ManaBar
+@onready var outline_highlighter: OutlineHighlighter = $OutlineHighlighter
+@onready var drag_and_drop: DragAndDrop = $DragAndDrop
+@onready var velocity_based_rotation: VelocityBasedRotation = $VelocityBasedRotation
+
+
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		drag_and_drop.drag_started.connect(_on_drag_started)
+		drag_and_drop.drag_canceled.connect(_on_drag_canceled)
+		drag_and_drop.dropped.connect(_on_dropped)
 
 
 func set_stats(value: UnitStats) -> void:
@@ -20,3 +31,39 @@ func set_stats(value: UnitStats) -> void:
 		await ready
 
 	skin.sprite_frames = stats.skin
+
+
+func reset_after_drag(starting_pos: Vector2) -> void:
+	velocity_based_rotation.enabled = false
+	global_position = starting_pos
+
+
+func _on_mouse_entered() -> void:
+	if drag_and_drop.dragging:
+		return
+
+	outline_highlighter.highlight()
+	z_index = 1
+
+
+func _on_mouse_exited() -> void:
+	if drag_and_drop.dragging:
+		return
+
+	outline_highlighter.clear_highlight()
+	z_index = 0
+
+
+func _on_drag_started() -> void:
+	shadow.visible = false
+	velocity_based_rotation.enabled = true
+
+
+func _on_drag_canceled(starting_pos: Vector2) -> void:
+	shadow.visible = true
+	reset_after_drag(starting_pos)
+
+
+func _on_dropped(_starting_pos: Vector2) -> void:
+	shadow.visible = true
+	velocity_based_rotation.enabled = false
